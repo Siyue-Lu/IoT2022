ca999fe87d43cdb2d09eedf191da911938d7b6f9
Yue <89789038+Siyue-Lu@users.noreply.github.com>
2022-12-16 15:25:34 +0100

    set up MQTT connection with Android Studio

--- app/src/main/java/com/example/iot2022group7/MainActivity.java
+++ app/src/main/java/com/example/iot2022group7/MainActivity.java
@@ -3,14 +3,23 @@ package com.example.iot2022group7;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.*;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.*;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView txv_temp_indoor = null;
+    TextView txv_outdoor_light = null;
     Switch lightToggle = null;
-    Button btnUpdateTemp = null;
+//    Button btnUpdateTemp = null;
+
+    private MqttAndroidClient client;
+    private static final String SERVER_URI = "tcp://test.mosquitto.org:1883";
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,24 +27,113 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.content_main);
 
         txv_temp_indoor = (TextView) findViewById(R.id.indoorTempShow);
+        txv_outdoor_light = (TextView) findViewById(R.id.outdoorLightShow);
+        lightToggle = (Switch) findViewById(R.id.btnToggle);
+//        btnUpdateTemp = (Button) findViewById(R.id.btnUpdateTemp);
         txv_temp_indoor.setText("the fetched indoor temp value");
 
-        lightToggle = (Switch) findViewById(R.id.btnToggle);
+        connect();
+        client.setCallback(new MqttCallbackExtended() {
+            @Override
+            public void connectComplete(boolean reconnect, String serverURI) {
+                if (reconnect) {
+                    System.out.println("Reconnected to : " + serverURI);
+                    // Re-subscribe as we lost it due to new session
+                } else {
+                    System.out.println("Connected to: " + serverURI);
+                }
+                subscribe("temp");
+            }
+
+            @Override
+            public void connectionLost(Throwable cause) {
+                System.out.println("The Connection was lost.");
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage message) throws Exception {
+                String newMessage = new String(message.getPayload());
+                System.out.println("Incoming message: " + newMessage);
+                /* add code here to interact with elements
+                (text views, buttons)
+                using data from newMessage
+                */
+                if (newMessage.length() == 4) {
+                    txv_temp_indoor.setText(newMessage);
+                }
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken token) {
+            }
+        });
+
+
         lightToggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
-
+                    txv_outdoor_light.setTextColor(getResources().getColor(R.color.teal_700));
+                    txv_outdoor_light.setText(R.string.txv_outdoor_light_on);
                 } else {
-
-                }}
-        });
-
-        btnUpdateTemp = (Button) findViewById(R.id.btnUpdateTemp);
-        btnUpdateTemp.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-
+                    txv_outdoor_light.setTextColor(getResources().getColor(R.color.title_bar_color));
+                    txv_outdoor_light.setText(R.string.txv_outdoor_light_show);
+                }
             }
         });
+
+//        btnUpdateTemp.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//
+//            }
+//        });
+    }
+
+    private void connect() {
+        String clientId = MqttClient.generateClientId();
+        client = new MqttAndroidClient(this.getApplicationContext(), SERVER_URI, clientId);
+        try {
+            IMqttToken token = client.connect();
+            token.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    // We are connected
+                    Log.d(TAG, "onSuccess");
+                    System.out.println(TAG + " Success. Connected to " + SERVER_URI);
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    // Something went wrong e.g. connection timeout or firewall problems
+                    Log.d(TAG, "onFailure");
+                    System.out.println(TAG + " Oh no! Failed to connect to " + SERVER_URI);
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void subscribe(String topicToSubscribe) {
+        final String topic = topicToSubscribe;
+        int qos = 1;
+        try {
+            IMqttToken subToken = client.subscribe(topic, qos);
+            subToken.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    System.out.println("Subscription successful to topic: " + topic);
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    System.out.println("Failed to subscribe to topic: " + topic);
+                    // The subscription could not be performed, maybe the user was not
+                    // authorized to subscribe on the specified topic e.g. using wildcards
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
     }
 }
