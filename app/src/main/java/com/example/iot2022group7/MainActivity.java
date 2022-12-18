0ee626b76dbffb0e086aabed346af3c546a0ec07
Yue <89789038+Siyue-Lu@users.noreply.github.com>
2022-12-18 12:45:26 +0100

    omitted MQTT
    completed light switching function with SSH+AsyncTask

--- app/src/main/java/com/example/iot2022group7/MainActivity.java
+++ app/src/main/java/com/example/iot2022group7/MainActivity.java
@@ -2,24 +2,30 @@ package com.example.iot2022group7;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
+import android.os.StrictMode;
 import android.view.View;
-import android.widget.*;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
 
-import org.eclipse.paho.android.service.MqttAndroidClient;
-import org.eclipse.paho.client.mqttv3.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import ch.ethz.ssh2.Connection;
+import ch.ethz.ssh2.Session;
+import ch.ethz.ssh2.StreamGobbler;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView txv_temp_indoor = null;
-    TextView txv_outdoor_light = null;
+    TextView outdoor_light_show = null;
     Switch lightToggle = null;
-//    Button btnUpdateTemp = null;
-
-    private MqttAndroidClient client;
-    private static final String SERVER_URI = "tcp://test.mosquitto.org:1883";
-    private static final String TAG = "MainActivity";
+    Button btnUpdateTemp = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,113 +33,86 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.content_main);
 
         txv_temp_indoor = (TextView) findViewById(R.id.indoorTempShow);
-        txv_outdoor_light = (TextView) findViewById(R.id.outdoorLightShow);
+        outdoor_light_show = (TextView) findViewById(R.id.outdoorLightShow);
         lightToggle = (Switch) findViewById(R.id.btnToggle);
-//        btnUpdateTemp = (Button) findViewById(R.id.btnUpdateTemp);
-        txv_temp_indoor.setText("the fetched indoor temp value");
+        btnUpdateTemp = (Button) findViewById(R.id.btnUpdateTemp);
 
-        connect();
-        client.setCallback(new MqttCallbackExtended() {
-            @Override
-            public void connectComplete(boolean reconnect, String serverURI) {
-                if (reconnect) {
-                    System.out.println("Reconnected to : " + serverURI);
-                    // Re-subscribe as we lost it due to new session
-                } else {
-                    System.out.println("Connected to: " + serverURI);
-                }
-                subscribe("temp");
-            }
-
-            @Override
-            public void connectionLost(Throwable cause) {
-                System.out.println("The Connection was lost.");
-            }
-
-            @Override
-            public void messageArrived(String topic, MqttMessage message) throws Exception {
-                String newMessage = new String(message.getPayload());
-                System.out.println("Incoming message: " + newMessage);
-                /* add code here to interact with elements
-                (text views, buttons)
-                using data from newMessage
-                */
-                if (newMessage.length() == 4) {
-                    txv_temp_indoor.setText(newMessage);
-                }
-            }
-
-            @Override
-            public void deliveryComplete(IMqttDeliveryToken token) {
-            }
-        });
+        // get light status and display in UI
 
 
+        // run scripts and change UI on toggle
         lightToggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
-                    txv_outdoor_light.setTextColor(getResources().getColor(R.color.teal_700));
-                    txv_outdoor_light.setText(R.string.txv_outdoor_light_on);
+                    new AsyncTask<Integer, Void, Void>() {
+                        @Override
+                        protected Void doInBackground(Integer... params) {
+                            run("python TurnOnLights.py");
+                            return null;
+                        }
+
+                        @Override
+                        protected void onPostExecute(Void v) {
+                            outdoor_light_show.setText(R.string.txv_on);
+                            outdoor_light_show.setTextColor(getResources().getColor(R.color.teal_700));
+                        }
+                    }.execute(1);
                 } else {
-                    txv_outdoor_light.setTextColor(getResources().getColor(R.color.title_bar_color));
-                    txv_outdoor_light.setText(R.string.txv_outdoor_light_show);
+                    new AsyncTask<Integer, Void, Void>() {
+                        @Override
+                        protected Void doInBackground(Integer... params) {
+                            run("python TurnOffLights.py");
+                            return null;
+                        }
+
+                        @Override
+                        protected void onPostExecute(Void v) {
+                            outdoor_light_show.setText(R.string.txv_off);
+                            outdoor_light_show.setTextColor(getResources().getColor(R.color.title_bar_color));
+                        }
+                    }.execute(1);
                 }
             }
         });
 
-//        btnUpdateTemp.setOnClickListener(new View.OnClickListener() {
-//            public void onClick(View v) {
-//
-//            }
-//        });
+        btnUpdateTemp.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+            }
+        });
     }
 
-    private void connect() {
-        String clientId = MqttClient.generateClientId();
-        client = new MqttAndroidClient(this.getApplicationContext(), SERVER_URI, clientId);
+    public void run(String command) {
+        String hostname = "7.tcp.eu.ngrok.io";
+        int port = 14536;
+        String username = "pi";
+        String password = "pi";
         try {
-            IMqttToken token = client.connect();
-            token.setActionCallback(new IMqttActionListener() {
-                @Override
-                public void onSuccess(IMqttToken asyncActionToken) {
-                    // We are connected
-                    Log.d(TAG, "onSuccess");
-                    System.out.println(TAG + " Success. Connected to " + SERVER_URI);
-                }
-
-                @Override
-                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
-                    // Something went wrong e.g. connection timeout or firewall problems
-                    Log.d(TAG, "onFailure");
-                    System.out.println(TAG + " Oh no! Failed to connect to " + SERVER_URI);
-                }
-            });
-        } catch (MqttException e) {
-            e.printStackTrace();
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+            Connection conn = new Connection(hostname, port); //init connection
+            conn.connect(); //start connection to the hostname
+            boolean isAuthenticated = conn.authenticateWithPassword(username, password);
+            if (isAuthenticated == false)
+                throw new IOException("Authentication failed.");
+            Session sess = conn.openSession();
+            sess.execCommand(command);
+            InputStream stdout = new StreamGobbler(sess.getStdout());
+            BufferedReader br = new BufferedReader(new InputStreamReader(stdout));//reads text
+            while (true) {
+                String line = br.readLine(); // read line
+                if (line == null)
+                    break;
+            }
+            /* Show exit status, if available (otherwise "null") */
+            System.out.println("ExitCode: " + sess.getExitStatus());
+            sess.close(); // Close this session
+            conn.close();
+        } catch (IOException e) {
+            e.printStackTrace(System.err);
+            System.exit(2);
         }
     }
 
-    private void subscribe(String topicToSubscribe) {
-        final String topic = topicToSubscribe;
-        int qos = 1;
-        try {
-            IMqttToken subToken = client.subscribe(topic, qos);
-            subToken.setActionCallback(new IMqttActionListener() {
-                @Override
-                public void onSuccess(IMqttToken asyncActionToken) {
-                    System.out.println("Subscription successful to topic: " + topic);
-                }
-
-                @Override
-                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
-                    System.out.println("Failed to subscribe to topic: " + topic);
-                    // The subscription could not be performed, maybe the user was not
-                    // authorized to subscribe on the specified topic e.g. using wildcards
-                }
-            });
-        } catch (MqttException e) {
-            e.printStackTrace();
-        }
-    }
 }
